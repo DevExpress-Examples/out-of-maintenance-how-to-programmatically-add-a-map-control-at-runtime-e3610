157621f224c9d9fea721801b8b654c08161269e7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Map;
+
+namespace DXMap_Runtime {
+
+    public partial class MainWindow : Window {
+        public MainWindow () {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded (object sender, RoutedEventArgs e) {
+            // Create a map control.
+            MapControl map = new MapControl();
+
+            // Create a layer.
+            ImageTilesLayer layer = new ImageTilesLayer();
+            map.Layers.Add(layer);
+
+            // Create a data provider.
+            OpenStreetMapDataProvider provider = new OpenStreetMapDataProvider();
+            layer.DataProvider = provider;
+
+            // Add the map control to the window.
+            this.Content = map;
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to programmatically add a map control at runtime
+
+
+<p>This example demonstrates how to add a MapControl at runtime.<br />
+</p>
+
+<br/>
+
+
