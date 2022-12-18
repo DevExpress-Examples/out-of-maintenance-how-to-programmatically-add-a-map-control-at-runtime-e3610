21e13f63dd8cfb7a6e70e06cbedc958ff04767d6
DevExpressExampleBot <fakeEmail>
2022-12-18 20:17:29 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,31 +1,24 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Xpf.Map
 
 Namespace DXMap_Runtime
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			' Create a map control and add it to the window.
-			Dim map As New MapControl()
-			Me.Content = map
-
-			' Create a layer.
-			Dim layer As New ImageLayer() With {
-				.DataProvider = New BingMapDataProvider() With {
-					.BingKey = "Insert your Bing Maps key here.",
-					.Kind = BingMapKind.Road
-				}
-			}
-			map.Layers.Add(layer)
-			map.ZoomLevel = 4
-			map.CenterPoint = New GeoPoint(40, -100)
-		End Sub
-
-	End Class
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ' Create a map control and add it to the window.
+            Dim map As MapControl = New MapControl()
+            Content = map
+            ' Create a layer.
+            Dim layer As ImageLayer = New ImageLayer() With {.DataProvider = New BingMapDataProvider() With {.BingKey = "Insert your Bing Maps key here.", .Kind = BingMapKind.Road}}
+            map.Layers.Add(layer)
+            map.ZoomLevel = 4
+            map.CenterPoint = New GeoPoint(40, -100)
+        End Sub
+    End Class
 End Namespace
